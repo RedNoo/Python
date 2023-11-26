25f4b39b7f3b908ae4a7b46a193b2d0d1e40f474
Onder Arslan <onder77@gmail.com>
2023-11-26 22:48:53 +0300

    roman to int answer finished

--- LeetCode/3_roman_to_integer.py
+++ LeetCode/3_roman_to_integer.py
@@ -3,22 +3,54 @@ class Solution:
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         x1 = {"I": "V,X", "X": "L,C", "C": "D,M"}
 
-        tempTotal = ""
+        temp_letter = ""
         val = []
-        total = 0
 
         for i in s:
+            print(i)
             if i in x1:
                 print("true")
-                print(i)
-            else:
-                print("false")
-                print(i)
+                if temp_letter == "" or temp_letter == i:
+                    temp_letter = i
+                    val.append(roman[i])
+                elif temp_letter == x1[i][0]:
+                    temp_letter = i
+                    val.append(roman[i])
+                elif temp_letter == x1[i][2]:
+                    temp_letter = i
+                    val.append(roman[i])
+                elif temp_letter not in x1 and i in x1:
+                    temp_letter = i
+                    val.append(roman[i])
+                elif i == x1[temp_letter][0] or i == x1[temp_letter][2]:
+                    val.append(roman[i] - 2 * roman[temp_letter])
+                    temp_letter = ""
+                else:
+                    temp_letter = i
+                    val.append(roman[i])
 
+
+            else:
+                if temp_letter == "":
+                    temp_letter = i
+                    val.append(roman[i])
+                elif temp_letter in x1:
+                    if i == x1[temp_letter][0] or i == x1[temp_letter][2]:
+                        val.append(roman[i] - 2*roman[temp_letter])
+                        temp_letter = ""
+                    else:
+                        temp_letter = i
+                        val.append(roman[i])
+                else:
+                    temp_letter = i
+                    val.append(roman[i])
+
+        print(temp_letter)
         total = sum(val)
 
         return total
 
 
 s = Solution()
-print(s.romanToInt("IV"))
+
+print(s.romanToInt("MCMXCIV"))
