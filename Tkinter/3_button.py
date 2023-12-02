8361ce657068e3ca2c6287ac8e273b526c4ed19e
Onder Arslan <onder77@gmail.com>
2023-12-02 18:38:34 +0300

    tkinter button added

--- Tkinter/3_button.py
+++ Tkinter/3_button.py
@@ -1,7 +1,29 @@
 from tkinter import *
 
+count = 0
+
+
+def click():
+    global count
+    count += 1
+    print(count)
+
 
 window = Tk()
 
+photo = PhotoImage(file='warehouse.png')
+
+button = Button(window, text="Click Me!",
+                command=click,
+                font=("Comis Sans", 30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE,
+                image=photo,
+                compound='bottom'
+                )
+button.pack()
 
 window.mainloop()
